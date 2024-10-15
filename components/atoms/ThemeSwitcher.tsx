1f903671955b17ad4ec1035d282c4fc50312e24c
koala819 <x.genolhac@gmail.com>
2024-10-15 11:52:14 +0200

    update:ThemeSwitcher

--- components/atoms/ThemeSwitcher.tsx
+++ components/atoms/ThemeSwitcher.tsx
@@ -1,53 +1,30 @@
-'use client'
-
 import { Moon, Sun } from 'lucide-react'
-import * as React from 'react'
+import { useState } from 'react'
 
 import { useTheme } from 'next-themes'
 
 import { Button } from '@/components/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
 
 export function ThemeSwitcher() {
-  const { setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false)
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode)
+    setTheme(theme === 'dark' ? 'light' : 'dark')
+  }
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="outline"
-          size="icon"
-          className="cursor-pointer hover:bg-accent hover:text-accent-foreground z-[60]"
-        >
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="z-[60] bg-white">
-        <DropdownMenuItem
-          onClick={() => setTheme('light')}
-          className="cursor-pointer hover:bg-gray-100 transition-colors"
-        >
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          onClick={() => setTheme('dark')}
-          className="cursor-pointer hover:bg-gray-100 transition-colors"
-        >
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          onClick={() => setTheme('system')}
-          className="cursor-pointer hover:bg-gray-100 transition-colors"
-        >
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button
+      variant="outline"
+      onClick={toggleDarkMode}
+      className=" p-2 bg-gray-200 dark:bg-gray-800 rounded-full shadow-lg"
+    >
+      {isDarkMode ? (
+        <Sun className="w-6 h-6 text-yellow-500" />
+      ) : (
+        <Moon className="w-6 h-6 text-gray-700" />
+      )}
+    </Button>
   )
 }
